c55502e0c1b5545bae15e5d36d84e11017f662bf
Arvin Lum <arvin.lum@hotmail.com>
2022-12-01 18:15:41 +0800

    automation is working, RSA Enc not ready

--- RSA_encryption.py
+++ RSA_encryption.py
@@ -1,23 +1,53 @@
-from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from pathlib import Path
 import os
 
-# Generating the keys
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    backend=default_backend()
-)
 
-public_key = private_key.public_key()
+class SetUp:
+    def __init__(self):
+        self.current_path = Path(__file__)
+        self.current_dir = self.current_path.parent.absolute()
 
-# Storing the public key into system env
-pem = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
-# os.environ['pubkey'] = pem.decode("utf-8").replace('\n', '\\n')
-print(os.environ['pubkey'])
-# with open('public_key.pem', 'wb') as f:
-#     f.write(pem)
+    def getCredentials(self):
+        return
+    
+    # Generating the keys
+    def keyGen(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+
+        self.public_key = self.private_key.public_key()
+
+    # Saving PK
+    def generatePEM(self):
+        pem = self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+        with open(self.current_dir+'\public_key.pem', 'wb') as f:
+            f.write(pem)
+
+    # Encrypt Credentials
+    '''
+    def Encrypting(self):
+
+
+        encrypted = self.public_key.encrypt(
+            credentials,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+    '''
+
+    def __str__(self) -> str:
+        return 'Current Path:\n{}\n\nCurrent Dir\n{}'.format(self.current_path, self.current_dir)
--- automation.py
+++ automation.py
@@ -2,63 +2,101 @@ from twilio.rest import Client
 from selenium import webdriver
 from time import sleep, ctime
 from random import randint
-import cryptography
-import datetime
+from RSA_encryption import SetUp
+import cryptography, datetime
 
-def notification(Twilio_SID, Auth_Token, msg, twilioPhone, phone):
-    account_sid = Twilio_SID
-    auth_token = Auth_Token
-    client = Client(account_sid, auth_token)
 
-    message = client.messages \
-                    .create(
-                        body=msg,
-                        from_='+' + twilioPhone,
-                        to='+' + phone
-                    )
+class Attendance_System:
+    def __init__(self, username, password, account_sid, auth_token, twilio_phone, usr_phone):
+        self.username = username
+        self.password = password
+        self.account_sid = account_sid
+        self.auth_token = auth_token
+        self.twilio_phone = twilio_phone
+        self.usr_phone = usr_phone
+    
+    def notification(self, msg):
+        Twilio_SID = self.account_sid
+        Auth_Token = self.auth_token
+        client = Client(Twilio_SID, Auth_Token)
 
-def main(username, password):
-    # # Go to FYPJ 2.0 Website
-    # driver = webdriver.Chrome()
-    # driver.get('https://fypj.sit.nyp.edu.sg/')
-    # driver.implicitly_wait(10)
+        message = client.messages \
+                        .create(
+                            body = msg,
+                            from_ = self.twilio_phone,
+                            to = self.usr_phone
+                        )
 
-    # # Enter Username
-    # driver.find_element('xpath','/html/body/div/div/div/div/div[2]/form/fieldset/div[2]/input').send_keys(username)
-    # # Enter Password
-    # driver.find_element('xpath','/html/body/div/div/div/div/div[2]/form/fieldset/div[3]/input').send_keys(password)
-    # driver.find_element('xpath','/html/body/div/div/div/div/div[2]/form/fieldset/div[6]/input[2]').click()
+    def automation(self):
+        # Go to FYPJ 2.0 Website
+        driver = webdriver.Chrome()
+        driver.get('https://fypj.sit.nyp.edu.sg/')
+        driver.implicitly_wait(10)
 
-    # # Click on Student Dropdown Button
-    # driver.find_element('xpath', '/html/body/form/div[3]/nav/div/div[2]/ul[1]/li[1]/a').click()
-    # # Click on Sign In/Out
-    # driver.find_element('xpath', '/html/body/form/div[3]/nav/div/div[2]/ul[1]/li[1]/ul/li[1]/a').click()
+        # Enter Username
+        driver.find_element('xpath','/html/body/div/div/div/div/div[2]/form/fieldset/div[2]/input').send_keys(self.username)
+        # Enter Password
+        driver.find_element('xpath','/html/body/div/div/div/div/div[2]/form/fieldset/div[3]/input').send_keys(self.password)
+        driver.find_element('xpath','/html/body/div/div/div/div/div[2]/form/fieldset/div[6]/input[2]').click()
 
-    # # Check In/Out of the system
-    # driver.find_element('xpath', '/html/body/form/div[3]/div[2]/div[5]/div/div/div[4]/div[3]/input').click()
-    # # Confirmation
-    # driver.find_element('xpath', '/html/body/form/div[3]/div[2]/div[8]/div/div/div[3]/button').click()
+        # Click on Student Dropdown Button
+        driver.find_element('xpath', '/html/body/form/div[3]/nav/div/div[2]/ul[1]/li[1]/a').click()
+        # Click on Sign In/Out
+        driver.find_element('xpath', '/html/body/form/div[3]/nav/div/div[2]/ul[1]/li[1]/ul/li[1]/a').click()
 
-    # Check In Notificaton
-    if datetime.time(8,24,0) <= datetime.datetime.now().time() <= datetime.time(8,30,59):
-        notification("You have Successfully Checked Into the FYPJ System at " + ctime())
-    elif datetime.time(5,59,59) <= datetime.datetime.now().time() <= datetime.time(6,6,0):
-        notification("You have Successfully Checked Out of the FYPJ System at " + ctime())
+        # Check In/Out of the system
+        driver.find_element('xpath', '/html/body/form/div[3]/div[2]/div[5]/div/div/div[4]/div[3]/input').click()
+        # Confirmation
+        # driver.find_element('xpath', '/html/body/form/div[3]/div[2]/div[8]/div/div/div[3]/button').click()
 
-    # return driver
-    return 'pog'
+        # Check In Notificaton
+        if datetime.time(8,25,0) <= datetime.datetime.now().time() <= datetime.time(8,30,59):
+            self.notification("You have Successfully Checked Into the FYPJ System at " + ctime())
+        # Check Out Notificaton
+        elif datetime.time(5,59,59) <= datetime.datetime.now().time() <= datetime.time(6,6,0):
+            self.notification("You have Successfully Checked Out of the FYPJ System at " + ctime())
+        
+        # self.msg = "Test Run - script is working as of " + ctime()
+        # self.notification()
+
+        # return driver
+        return driver
+
 
 if __name__ == '__main__':
+    # '''
+    while True:
+        current_time = datetime.datetime.now()
 
+        # Check if today is Monday - Friday
+        if current_time.isoweekday() in range(1, 6):
+            # Trigger Function at 8.20 am
+            if datetime.time(8,20,0) <= datetime.datetime.now().time() <= datetime.time(8,30,59):
+                # Emulate human's randomness (Up to 4mins)  
+                sleep(randint(1,240))
+                try:
+                    driver = Attendance_System().automation() 
+                except:
+                    Attendance_System.notification("You have Failed to Check In of the FYPJ System at " + ctime())
 
-    
-        
+                sleep(21600)
+            # Trigger Function at 6:00 pm
+            elif datetime.time(18,0,0) <= datetime.datetime.now().time() <= datetime.time(6,6,0):
+                # Emulate human's randomness (Up to 4mins)  
+                sleep(randint(1,240))
+                try:
+                    driver = Attendance_System().automation() 
+                except:
+                    Attendance_System.notification("You have Failed to Check Out of the FYPJ System at " + ctime())
 
+                sleep(21600)
+            # Stop the script for 5 mins
+            else:
+                sleep(300)     
+        # Stop Running the code on Saturday and Sunday by pausing the code for 6 hours:
+        else:
+            sleep(21600)
+            continue
 
-    # Emulate human's randomness (Up to 4mins)
-    # sleep(randint(1,240))
-    # try:
-    #     driver = main()
-    # except:
-    #     notification("You have Failed to Check In/Out of the FYPJ System at " + ctime())
-        
+    # '''
+
