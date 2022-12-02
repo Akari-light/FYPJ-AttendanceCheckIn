4ffd8d242e5115b9b4a026d3b1dc2b29da50b07f
Arvin Lum <arvin.lum@hotmail.com>
2022-12-02 13:05:29 +0800

    added encryption

--- automation.py
+++ automation.py
@@ -1,10 +1,41 @@
+from cryptography.fernet import Fernet
 from twilio.rest import Client
 from selenium import webdriver
 from time import sleep, ctime
 from random import randint
-from RSA_encryption import SetUp
-import cryptography, datetime
+from json import dumps, loads
+from pathlib import Path
+import cryptography, datetime, os
 
+class SetUp:
+    def __init__(self):
+        self.file_path = Path(__file__)
+        self.current_dir = self.file_path.parent.absolute()
+        self.retriveCredentials()
+
+    # Retrive user creds
+    def retriveCredentials(self):
+        reset_msg = "username     : username\npassword     : password\naccount_sid  : account_sid\nauth_token   : auth_token\ntwilio_phone : twilio_phone\nusr_phone    : usr_phone"
+        self.credentials = {}
+        with open(str(self.current_dir)+'\credentials.txt', 'r') as f:
+            creds_list= f.read().split('\n')
+            for i in creds_list:
+                self.credentials[i.split(":")[0].strip(' ')] = i.split(":")[1].strip(' ')
+
+        # Reset credentials.txt
+        with open(str(self.current_dir)+'\credentials.txt', 'w') as f:
+            f.write(reset_msg)
+   
+    def encryption(self):
+        plaintext = dumps(self.credentials).encode('utf-8')
+        key = Fernet.generate_key()
+
+        self.token = Fernet(key)
+        self.credentials = self.token.encrypt(plaintext)
+
+    def getCredentials(self):
+        plaintext = loads(self.token.decrypt(self.credentials).decode('utf-8'))
+        return plaintext
 
 class Attendance_System:
     def __init__(self, username, password, account_sid, auth_token, twilio_phone, usr_phone):
@@ -58,17 +89,41 @@ class Attendance_System:
         elif datetime.time(5,59,59) <= datetime.datetime.now().time() <= datetime.time(6,6,0):
             self.notification("You have Successfully Checked Out of the FYPJ System at " + ctime())
         
-        '''
-        self.msg = "Test Run - script is working as of " + ctime()
-        self.notification()
-        '''
+        # '''
+        self.notification("Test Run - script is working as of " + ctime())
+        # '''
 
         # return driver
         return driver
 
 
 if __name__ == '__main__':
-    # '''
+    #Setup for script
+    user_instance = SetUp()
+    user_instance.encryption()
+    credentials = user_instance.getCredentials()
+    system_instance = Attendance_System(credentials["username"], credentials["password"],credentials["account_sid"],credentials["auth_token"],credentials["twilio_phone"],credentials["usr_phone"])
+    
+    while True:
+        current_time = datetime.datetime.now()
+
+        # Check if today is Monday - Friday
+        if current_time.isoweekday() in range(1, 6):
+            if datetime.time(13,0,0) <= datetime.datetime.now().time() <= datetime.time(13,59,59):
+                driver = system_instance.automation() 
+                print("Working")
+                quit()
+            # Stop the script for 5 mins
+            else:
+                sleep(300)     
+        # Stop Running the code on Saturday and Sunday by pausing the code for 6 hours:
+        else:
+            sleep(21600)
+            continue
+
+
+
+    '''
     while True:
         current_time = datetime.datetime.now()
 
@@ -79,21 +134,21 @@ if __name__ == '__main__':
                 # Emulate human's randomness (Up to 4mins)  
                 sleep(randint(1,240))
                 try:
-                    driver = Attendance_System().automation() 
+                    driver = system_instance.automation() 
                 except:
-                    Attendance_System.notification("You have Failed to Check In of the FYPJ System at " + ctime())
+                    system_instance.notification("You have Failed to Check In of the FYPJ System at " + ctime())
 
-                sleep(21600)
+                # sleep(21600)
             # Trigger Function at 6:00 pm
             elif datetime.time(18,0,0) <= datetime.datetime.now().time() <= datetime.time(6,6,0):
                 # Emulate human's randomness (Up to 4mins)  
                 sleep(randint(1,240))
                 try:
-                    driver = Attendance_System().automation() 
+                    driver = system_instance.automation() 
                 except:
-                    Attendance_System.notification("You have Failed to Check Out of the FYPJ System at " + ctime())
+                    system_instance.notification("You have Failed to Check Out of the FYPJ System at " + ctime())
 
-                sleep(21600)
+                # sleep(21600)
             # Stop the script for 5 mins
             else:
                 sleep(300)     
@@ -101,5 +156,4 @@ if __name__ == '__main__':
         else:
             sleep(21600)
             continue
-    # '''
-
+    '''
